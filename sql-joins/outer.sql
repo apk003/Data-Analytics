d6583d0e1f2ea1bc0e88f6589284dd5d874fa1fb
Anthony <96312933+apk003@users.noreply.github.com>
2023-06-05 11:32:42 -0500

    Add files via upload

---
+++ sql-joins/outer.sql
@@ -0,0 +1,11 @@
+SELECT
+  employees.name AS employee_name,
+  employees.role AS employee_role,
+  departments.name AS department_name
+FROM
+  employee_data.employees
+FULL OUTER JOIN
+  employee_data.departments ON
+  employees.department_id = departments.department_id
+ORDER BY 
+  employee_name
